ee3ffc200e5f8e79c9a8aa7dbfa81bf9eadffaab
Shiqi Zhang <shiqi@unc.edu>
2021-04-08 03:12:41 -0400

    completed homepage

---
+++ scripts/navbar.js
@@ -0,0 +1,8 @@
+var toggleButton = document.querySelector(".toggle");
+var link = document.querySelector(".nav-link");
+
+function toggle() {
+    link.classList.toggle("active");
+}
+
+toggleButton.addEventListener("click", toggle);
